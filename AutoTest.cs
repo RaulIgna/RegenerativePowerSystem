0cc5ce4242666a6b99f7105f4c70fe3dc235e516
Raul Igna <ignaraul12@yahoo.com>
2024-03-11 17:58:51 +0200

    Add project files.

---
+++ AutoTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegenerativePowerSystem
+{
+    public class Error
+    {
+        private bool oK = false;
+        private string serror = (string)null;
+
+        public Error()
+        {
+            this.oK = false;
+            this.serror = "Default error";
+        }
+        public Error(string sError)
+        {
+            oK = false;
+            serror = sError;
+        }
+        public Error(bool bOk, string sError)
+        {
+            this.oK = bOk;
+            this.serror = sError;
+        }
+
+        public bool OK
+        {
+            get => this.oK;
+            set => this.oK = value;
+        }
+
+        public string Str_Error
+        {
+            set => this.Set_Error_String(value);
+            get => this.serror;
+        }
+
+        public void Set_Error_String(string sError)
+        {
+            lock (this)
+            {
+                this.oK = false;
+                this.serror = sError;
+            }
+        }
+    } 
+    
+}
---
+++ RegenrativePowerSystem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agilent.AgAPS.Interop;
+
+namespace RegenerativePowerSystem
+{
+    public class DMMASP
+    {
+        public string ResourceName { get; set; }
+        public string ID { get; set; }
+        public bool Run {  get; set; }
+        public double OVPLimit { get; set; }
+        public double VoltageLimit { get; set; }
+
+        public ThreadStaticAttribute Work { get; set; }
+        public AgAPS Driver {  get; set; }
+
+        public DMMASP (string id)
+        {
+            ResourceName = "USB0::0x2A8D::0x2802::MY6300" + id + "::0::INSTR";
+            ID = id;
+            Run = true;
+        }
+    }
+
+    public class RegenrativePowerSystem
+    {
+        public static Error Open(DMMASP DMM)
+        {
+            // Try to close the driver
+            try
+            {
+                DMM.Driver.Close();
+                DMM.Driver = null;
+            }
+            catch{ }
+
+            Error er = new Error();
+            try
+            {
+                DMM.Driver = new AgAPS();
+                DMM.Driver.Initialize(DMM.ResourceName, false, false);
+                // TODO: Handle errors
+
+
+            }
+            catch(Exception e) 
+            {
+                DMM.Run = false;
+                return new Error(e.Message);
+            }
+            return er;
+        }
+    }
+}
